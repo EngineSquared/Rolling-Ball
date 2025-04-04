d2c993a9be12636771139134e613040563a2cc32
Miouzora <alexandre.franquet@epitech.eu>
2025-04-04 23:39:16 +0900

    refactor: move game scene into an appropriate scene

---
+++ src/scene/GameScene.hpp
@@ -0,0 +1,175 @@
+#include "Scene.hpp"
+
+#include "PlayerJump.hpp"
+#include "PlayerMovement.hpp"
+#include "PointCameraToPlayer.hpp"
+#include "SpawnPlayer.hpp"
+#include "Core.hpp"
+
+#include <Jolt/RegisterTypes.h>
+#include <Jolt/Physics/Collision/Shape/BoxShape.h>
+
+using namespace ES::Plugin;
+
+static ES::Engine::Entity CreateFloor(ES::Engine::Core &core)
+{
+	glm::vec3 floor_position = glm::vec3(0.0f, -8.0f, 0.0f);
+	glm::vec3 floor_scale = glm::vec3(1.0f, 1.0f, 1.0f);
+
+	// Rotated slightly in a 12° angle, so that the sphere will roll on it
+	glm::quat floor_rotation = glm::rotate(glm::quat(0.0f, 0.0f, 0.0f, 1.0f), glm::radians(12.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+	JPH::Vec3 floor_size = JPH::Vec3(10.0f, 1.0f, 10.0f);
+
+	std::shared_ptr<JPH::BoxShapeSettings> floor_shape_settings = std::make_shared<JPH::BoxShapeSettings>(floor_size);
+	ES::Engine::Entity floor = core.CreateEntity();
+
+	floor.AddComponent<Object::Component::Transform>(core, floor_position, floor_scale, floor_rotation);
+	floor.AddComponent<Physics::Component::RigidBody3D>(core, floor_shape_settings, JPH::EMotionType::Static, Physics::Utils::Layers::NON_MOVING);
+
+	// Add a mesh to it for rendering
+	floor.AddComponent<OpenGL::Component::ShaderHandle>(core, "default");
+    floor.AddComponent<OpenGL::Component::MaterialHandle>(core, "default");
+    floor.AddComponent<OpenGL::Component::ModelHandle>(core, "floor");
+
+    Object::Component::Mesh mesh;
+	{
+		// Generate a box
+		const float width = floor_size.GetX();
+		const float height = floor_size.GetY();
+		const float depth = floor_size.GetZ();
+
+		// Generate vertices and normals
+		const glm::vec3 front_bottom_left = glm::vec3(-width, -height, -depth);
+		const glm::vec3 front_bottom_right = glm::vec3(width, -height, -depth);
+		const glm::vec3 front_top_left = glm::vec3(-width, height, -depth);
+		const glm::vec3 front_top_right = glm::vec3(width, height, -depth);
+		const glm::vec3 back_bottom_left = glm::vec3(-width, -height, depth);
+		const glm::vec3 back_bottom_right = glm::vec3(width, -height, depth);
+		const glm::vec3 back_top_left = glm::vec3(-width, height, depth);
+		const glm::vec3 back_top_right = glm::vec3(width, height, depth);
+
+		// Front face
+		mesh.vertices.push_back(front_bottom_left);
+		mesh.vertices.push_back(front_bottom_right);
+		mesh.vertices.push_back(front_top_left);
+		mesh.vertices.push_back(front_top_right);
+
+		mesh.normals.push_back(glm::vec3(0.0f, 0.0f, -1.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, 0.0f, -1.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, 0.0f, -1.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, 0.0f, -1.0f));
+
+		// Back face
+		mesh.vertices.push_back(back_bottom_left);
+		mesh.vertices.push_back(back_bottom_right);
+		mesh.vertices.push_back(back_top_left);
+		mesh.vertices.push_back(back_top_right);
+
+		mesh.normals.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
+
+		// Top face
+		mesh.vertices.push_back(front_top_left);
+		mesh.vertices.push_back(front_top_right);
+		mesh.vertices.push_back(back_top_left);
+		mesh.vertices.push_back(back_top_right);
+
+		mesh.normals.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
+
+		// Bottom face
+		mesh.vertices.push_back(front_bottom_left);
+		mesh.vertices.push_back(front_bottom_right);
+		mesh.vertices.push_back(back_bottom_left);
+		mesh.vertices.push_back(back_bottom_right);
+
+		mesh.normals.push_back(glm::vec3(0.0f, -1.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, -1.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, -1.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(0.0f, -1.0f, 0.0f));
+
+		// Left face
+		mesh.vertices.push_back(front_bottom_left);
+		mesh.vertices.push_back(front_top_left);
+		mesh.vertices.push_back(back_bottom_left);
+		mesh.vertices.push_back(back_top_left);
+
+		mesh.normals.push_back(glm::vec3(-1.0f, 0.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(-1.0f, 0.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(-1.0f, 0.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(-1.0f, 0.0f, 0.0f));
+
+		// Right face
+		mesh.vertices.push_back(front_bottom_right);
+		mesh.vertices.push_back(front_top_right);
+		mesh.vertices.push_back(back_bottom_right);
+		mesh.vertices.push_back(back_top_right);
+
+		mesh.normals.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+		mesh.normals.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+
+		// Generate indices for triangle strips
+
+		mesh.indices = {
+			// Front
+			2, 1, 0,
+			2, 3, 1,
+			// Back
+			4, 5, 6,
+			5, 7, 6,
+			// Bottom
+			10, 9, 8,
+			10, 11, 9,
+			// Top
+			12, 13, 14,
+			13, 15, 14,
+			// Left
+			18, 17, 16,
+			18, 19, 17,
+			// Right
+			20, 21, 22,
+			21, 23, 22,
+		};
+	}
+	floor.AddComponent<Object::Component::Mesh>(core, mesh);
+	floor.AddComponent<Game::Terrain>(core);
+
+
+	return floor;
+}
+
+namespace Game
+{
+    class GameScene : public ES::Plugin::Scene::Utils::AScene {
+        inline static int _numScenes = 0;
+    
+      public:
+        GameScene() : ES::Plugin::Scene::Utils::AScene() {}
+    
+      protected:
+        void _onCreate(ES::Engine::Core &core) final
+        {
+            CreateFloor(core);
+            core.RegisterSystem<ES::Engine::Scheduler::Update>(
+                Game::PlayerJump
+            );
+            core.RegisterSystem<ES::Engine::Scheduler::FixedTimeUpdate>(
+                Game::PointCameraToPlayer,
+                Game::PlayerMovement
+            );
+            Game::SpawnPlayer(core);
+        }
+    
+        void _onDestroy(ES::Engine::Core &core) final
+        {
+            
+        }
+    };
+}
